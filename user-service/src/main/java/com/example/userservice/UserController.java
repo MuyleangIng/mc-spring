f87ace8821533d64a2ea6bbb4813c81ed0d75895
sen <sen@sens-Mac-mini.local>
2024-09-02 13:16:54 +0700

    update

--- user-service/src/main/java/com/example/userservice/UserController.java
+++ user-service/src/main/java/com/example/userservice/UserController.java
@@ -1,15 +1,21 @@
 package com.example.userservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RefreshScope
 @RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Value("${user.test.property:default-value}")
+    private String testProperty;
 
     @PostMapping
     public User createUser(@RequestBody User user) {
@@ -19,5 +25,10 @@ public class UserController {
     @GetMapping
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/test-property")
+    public String getTestProperty() {
+        return "Test Property: " + testProperty;
     }
 }
